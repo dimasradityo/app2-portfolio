3b63e1610fbf8b2d737c7580d58c55aa6df1eeec
Dimas Radityo <dimsrad307@gmail.com>
2024-11-28 21:33:55 +0700

    Add description, image and links

--- main.py
+++ main.py
@@ -21,14 +21,20 @@ built in Python"
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
 
 df = pd.read_csv('data.csv', sep=';')
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.image(f"images/{row['image']}")
+        st.write(row['description'])
+        st.write(f"[Link to Repository]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.image(f"images/{row['image']}")
+        st.write(row['description'])
+        st.write(f"[Link to Repository]({row['url']})")
