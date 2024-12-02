dd1cf517d9e46ef627b193a422a1ff825208cb82
Dimas Radityo <dimsrad307@gmail.com>
2024-12-02 16:46:02 +0700

    Added news aggregator project

--- pages/1_Personal_Projects.py
+++ pages/1_Personal_Projects.py
@@ -7,3 +7,9 @@ with st.container(border=True):
     st.image('images/personal_projects/resume.png')
     st.write('Importing my static PDF resume to a Python web app.')
     st.write(f"[Source Code](https://github.com/dimasradityo/resume-dimas-radityo)")
+
+with st.container(border=True):
+    st.header('[News Aggregator](https://dimas-news-aggregator.streamlit.app/)')
+    st.image('images/personal_projects/news_aggregator.png')
+    st.write("Simple news aggregator to search for news based on user's search query. Data source is using API fron newsapi.org.")
+    st.write(f"[Source Code](https://github.com/dimasradityo/news_aggregator)")
