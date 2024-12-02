c432db85367fb6dd4de4517d1b88bc5c263c1c71
Dimas Radityo <dimsrad307@gmail.com>
2024-12-02 12:01:58 +0700

    Added app link for resume project

--- pages/Personal_Projects.py
+++ pages/Personal_Projects.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('Personal Projects')
 
 with st.container(border=True):
-    st.header('Resume')
+    st.header('[Resume](https://resumedimasradityo.streamlit.app/)')
     st.image('images/personal_projects/resume.png')
     st.write('Importing my static PDF resume to a Python web app.')
     st.write(f"[Source Code](https://github.com/dimasradityo/resume-dimas-radityo)")
