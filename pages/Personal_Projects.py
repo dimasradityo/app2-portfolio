9e99cf1af9c4010c4d5f0c6f548fd5ef15b9cca0
Dimas Radityo <dimsrad307@gmail.com>
2024-12-02 11:44:39 +0700

    Edited personal projects page

--- pages/Personal_Projects.py
+++ pages/Personal_Projects.py
@@ -5,5 +5,5 @@ st.title('Personal Projects')
 with st.container(border=True):
     st.header('Resume')
     st.image('images/personal_projects/resume.png')
-    st.write('Importing my static resume to a Python web app.')
+    st.write('Importing my static PDF resume to a Python web app.')
     st.write(f"[Source Code](https://github.com/dimasradityo/resume-dimas-radityo)")
